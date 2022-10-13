f26a2e35e8e4cc6d68b6ef3e3ef6b737c4859c4f
EmanTahir <emantahir555@example.com>
2022-10-13 16:08:28 +0500

    useEffect Hook added

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import Header from "./MyComponents/Header";
-import {Footer} from "./MyComponents/Footer";
-import {Todos} from "./MyComponents/Todos";
-import {AddTodo} from "./MyComponents/AddTodo";
-import React, {useState} from 'react';
+import { Footer } from "./MyComponents/Footer";
+import { Todos } from "./MyComponents/Todos";
+import { AddTodo } from "./MyComponents/AddTodo";
+import React, { useState, useEffect } from "react";
 
 function App() {
   let initTodo;
-  if(localStorage.getItem("todos")===null){
+  if (localStorage.getItem("todos") === null) {
     initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
   }
-  else{
-    initTodo=JSON.parse(localStorage.getItem("todos"));
-  }
-
 
   const onDelete = (todo) => {
     console.log("I am on Delete of todo: ", todo);
-    setTodos(todos.filter((e)=> {
-      return e !==todo;
-    }));
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
     localStorage.setItem("todos", JSON.stringify(todos));
-  }
-  const addTodo= (title, desc)=>{
+  };
+  const addTodo = (title, desc) => {
     console.log("I am ading this todo: ", title, desc);
     let sno;
-    if(todos.length ===0){
+    if (todos.length === 0) {
       sno = 1;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
     }
-    else{
-      sno = todos[todos.length-1].sno + 1;
-    }
-    
+
     const myTodo = {
       sno: sno,
       title: title,
       description: desc,
     };
-    console.log(myTodo)
+    console.log(myTodo);
     setTodos([...todos, myTodo]);
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
+  };
   const [todos, setTodos] = useState(initTodo);
-  
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <>
-    <Header title = "Todo List"  />
-    <AddTodo addTodo = {addTodo} />
-    <Todos todos={todos} onDelete= {onDelete} />
-    <Footer />
-  </>
+      <Header title="Todo List" />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
